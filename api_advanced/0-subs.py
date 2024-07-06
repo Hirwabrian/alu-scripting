d8c1438482aef05ec7f6f82ae30df50e01521d90
Hirwabrian <156072668+Hirwabrian@users.noreply.github.com>
2024-07-06 20:09:07 +0200

    0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
-"""
-number of subscribers for a given subreddit
-"""
+"""Module for task 0"""
 
-from requests import get
-
+import requests
 
 def number_of_subscribers(subreddit):
-    """
-    function that queries the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
-    """
-
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
-
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
-    results = response.json()
-
+    """Queries the Reddit API and returns the number of subscribers"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"  
+    headers = {"User-Agent": "Hbrian"}
+    
     try:
-        return results.get('data').get('subscribers')
-
-    except Exception:
-        return 0
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        
+        if response.status_code == 200:
+            data = response.json()
+            subscribers = data['data']['subscribers']
+            return subscribers  
+        else:
+            return None  
+    
+    except Exception as error:
+        print(f"An error occurred: {error}")
+        return None
