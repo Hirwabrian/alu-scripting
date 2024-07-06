c8537879f961bf02206bc51a9f4c5263e5a62916
Hirwabrian <h.brian@alustudent.com>
2024-07-06 07:32:55 -0700

    adding documentation

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -5,6 +5,7 @@ Script to fetch the number of subscribers for a given subreddit.
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     """
     Fetches the number of subscribers for a given subreddit.
@@ -18,16 +19,16 @@ def number_of_subscribers(subreddit):
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "myRedditApp/0.0.1"}
-    
+
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
-        
+
         if response.status_code == 200:
             data = response.json()
             return data["data"]["subscribers"]
         else:
             return 0
-    
+
     except Exception as e:
         print(f"An error occurred: {e}")
         return 0
--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,6 +5,7 @@ Script that queries the Reddit API and prints the titles of the first 10 hot pos
 
 import requests
 
+
 def top_ten(subreddit):
     """
     Queries the Reddit API and prints the titles of the first 10 hot posts for a given subreddit.
@@ -18,14 +19,14 @@ def top_ten(subreddit):
     """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {"User-Agent": "myRedditApp/0.0.1"}
-    
+
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         data = response.json()
         posts = data['data']['children']
-        
+
         for post in posts:
             print(post['data']['title'])
-    
+
     except Exception as error:
         print(None)
