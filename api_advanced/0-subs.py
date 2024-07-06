3c9bb1f7dea9a6577af3023b6a36aa5eb8b4c6f8
Hirwabrian <156072668+Hirwabrian@users.noreply.github.com>
2024-07-06 16:13:03 +0200

    0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -9,9 +9,10 @@ def number_of_subscribers(subreddit):
     headers = {"User-Agent": "myRedditApp/0.0.1"}
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code != 200:
+        if response.status_code = 200:
+            data = response.json()
+            return data["data"]["subscribers"]
+        else:
             return 0
-        data = response.json()
-        return data["data"]["subscribers"]
     except Exception:
         return 0
