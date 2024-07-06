4a1a2649c0250d51736ee17c6a82edc1f526eca5
Hirwabrian <156072668+Hirwabrian@users.noreply.github.com>
2024-07-06 16:26:50 +0200

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,19 +1,31 @@
 #!/usr/bin/python3
-
 """
-Script that Queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
+Script that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
 """
 
 import requests
+
 def top_ten(subreddit):
+    """
+    Queries the Reddit API and prints the titles of the first 10 hot posts for a given subreddit.
+
+    Args:
+    - subreddit (str): The name of the subreddit (without '/r/').
+
+    Returns:
+    - None: Prints the titles of the top 10 posts.
+            If the subreddit doesn't exist or there's an error, prints None.
+    """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {
-        'User-Agent': 'myRedditApp/0.0.1' }
+    headers = {"User-Agent": "myRedditApp/0.0.1"}
+    
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         data = response.json()
         posts = data['data']['children']
+        
         for post in posts:
             print(post['data']['title'])
+    
     except Exception as error:
         print(None)
