c0fe4ba8c5025d38a5bea930bf9117fb319da310
Hirwabrian <156072668+Hirwabrian@users.noreply.github.com>
2024-07-06 14:44:54 +0200

    0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -3,14 +3,15 @@
 script that returns the number of subscribers (not active users, total subscribers) for a given subreddit
 """
 
-
+import requests
 def number_of_subscribers(subreddit):
-    import requests
-    req = requests.get("https://api.reddit.com/r/{}/about.json"
-                       .format(subreddit),
-                       headers={"User-Agent": "MyRedditBot/0.0.1"})
-    req = req.json()
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "myRedditApp/0.0.1"}
     try:
-        return req["data"]["subscribers"]
-    except Exception as error:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code != 200:
+            return 0
+        data = response.json()
+        return data["data"]["subscribers"]
+    except Exception:
         return 0
