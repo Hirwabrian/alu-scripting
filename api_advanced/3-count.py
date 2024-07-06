89d1e0535c62042a16ad423d5398b902fddd36b5
Hirwabrian <156072668+Hirwabrian@users.noreply.github.com>
2024-07-06 19:48:14 +0200

    3-count.py

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -14,7 +14,7 @@ def count_words(subreddit, word_list, after="", count=[]):
     request = requests.get(url,
                            params={'after': after},
                            allow_redirects=False,
-                           headers={'user-agent': 'bhalut'})
+                           headers={'user-agent': 'Hbrian'})
 
     if request.status_code == 200:
         data = request.json()
