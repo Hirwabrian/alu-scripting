4612fbd1cae37ddab32902985a37d76bcb61a720
Hirwabrian <156072668+Hirwabrian@users.noreply.github.com>
2024-07-06 19:20:35 +0200

    0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -7,15 +7,20 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "myRedditApp/0.0.1"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """
+    Return the total number of subscribers on a given subreddit..
+    """
+
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    user_agent = {'User-agent': 'myRedditApp/0.0.1'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
+
     try:
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-    except Exception as error:
+        return results.get('data').get('subscribers')
+
+    except Exception:
         return 0
